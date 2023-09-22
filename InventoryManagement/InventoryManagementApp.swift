2ee638d06e24f5b6f3a304bd58c982f5bfec9584
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-09-22 00:53:32 -0300

    Initial Commit

---
+++ InventoryManagement/InventoryManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryManagementApp.swift
+//  InventoryManagement
+//
+//  Created by Christian Manzaraz on 22/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct InventoryManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
